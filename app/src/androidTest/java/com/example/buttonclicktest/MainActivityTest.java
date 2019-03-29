4328b40b900bfc4db20c6b636980f62909a8fd16
Xavier <xavierai@qq.com>
2019-03-29 19:57:22 +0800

    test

---
+++ app/src/androidTest/java/com/example/buttonclicktest/MainActivityTest.java
@@ -0,0 +1,46 @@
+package com.example.buttonclicktest;
+
+import android.support.test.espresso.Espresso;
+import android.support.test.rule.ActivityTestRule;
+import android.util.Log;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.Rule;
+
+import static org.junit.Assert.*;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
+
+public class MainActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> activityTestRule = new ActivityTestRule<>(MainActivity.class);
+    public MainActivity mainActivity = null;
+
+    @Before
+    public void setUp() throws Exception {
+        mainActivity = activityTestRule.getActivity();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void onCreate() {
+        Log.d("TAG","onCreate()");
+    }
+
+    @Test
+    public void btn_click() {
+        Log.d("TAG","Button was click");
+    }
+
+    @Test
+    public void testClickButton(){
+        Espresso.onView(withId(R.id.btn)).perform(click());
+    }
+}
